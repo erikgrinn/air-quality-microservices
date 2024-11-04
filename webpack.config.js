03ccb8ee3e110d7324663b9b498ff181b8c91b10
Erik Grinn <grinn.erik@gmail.com>
2024-11-04 01:38:05 -0500

    removed copywebpackplugin from webpack.config.js, and removed dist from gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 // webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: "development",
