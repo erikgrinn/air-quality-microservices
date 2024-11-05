4512aa862fb81d5073bd0456c855e2480d1d96ba
Erik Grinn <grinn.erik@gmail.com>
2024-11-04 19:46:34 -0500

    removed commented zeromq workarounds

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 // webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const webpack = require('webpack'); // ZEROMQ
 
 module.exports = {
   mode: "development",
@@ -11,22 +10,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  // resolve: { // ZEROMQ
-  //   fallback: {
-  //       "fs": false, // Ignore the `fs` module
-  //       "path": require.resolve("path-browserify") // Optionally add path-browserify for compatibility
-  //   }
-  // },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
-    // new webpack.IgnorePlugin({ // ZEROMQ
-    //   resourceRegExp: /zeromq|load-addon\.js/,
-    // }),
   ],
   module: {
-    // noParse: /zeromq|load-addon\.js/, // ZEROMQ Ignore parsing for ZeroMQ
     rules: [
       {
         test: /\.css$/i,
