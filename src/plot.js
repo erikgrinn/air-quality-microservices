461cd98b6d008d77391b6fc5ee0e4ffa72c9b485
Erik Grinn <grinn.erik@gmail.com>
2024-11-07 20:26:27 -0500

    plot.js initial

--- src/plot.js
+++ src/plot.js
@@ -1,2 +1,21 @@
+// import './styles.css';
+
 import * as Plot from "@observablehq/plot";
 
+import { csvParse } from 'd3-dsv';
+
+async function loadData() {
+    const response = await fetch('./files/cities_air_quality_water_pollution.18-10-2021.csv');
+    const text = await response.text();
+    const data = csvParse(text);
+    return data;
+}
+
+loadData().then(data => {
+    const chart = Plot.plot({
+        marks: [
+            Plot.barY(data, { x: "Regions", y: "AirQuality" })
+        ]
+    });
+    document.querySelector('.header').appendChild(chart);
+});
